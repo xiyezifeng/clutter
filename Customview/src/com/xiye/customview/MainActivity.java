8a4e11046a5af5d4fb7ed1966d3d356ff38c52ef
xiyezifeng <xiyezifeng@163.com>
2015-11-05 17:56:52 +0800

    viewpager 切换动画

--- Customview/src/com/xiye/customview/MainActivity.java
+++ Customview/src/com/xiye/customview/MainActivity.java
@@ -1,36 +1,60 @@
 package com.xiye.customview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.view.ViewPager;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 
+import com.xiye.imagepager.ImagePager;
+import com.xiye.imagepager.MyPagerAdapter;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	private ImagePager imagePager;
 
+	private ViewPager mViewPager;
+	private int[] mImgIds = new int[] {
+			R.drawable.image_1, R.drawable.image_2,
+			R.drawable.image_3, R.drawable.image_4,
+			R.drawable.image_5, R.drawable.image_6,
+			R.drawable.image_7, R.drawable.image_8,
+			R.drawable.image_9, R.drawable.image_10,
+			};
+	private List<ImageView> mImageViews = new ArrayList<ImageView>();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		selectePager(0);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private void initData() {
+		for (int imgId : mImgIds) {
+			ImageView imageView = new ImageView(getApplicationContext());
+			imageView.setScaleType(ScaleType.CENTER_CROP);
+			imageView.setImageResource(imgId);
+			mImageViews.add(imageView);
+		}
+	}
+
+	private void selectePager(int position) {
+
+		switch (position) {
+		case 0:
+			imagePager = (ImagePager) findViewById(R.id.viewpager);
+			initData();
+			imagePager.setAdapter(new MyPagerAdapter(mImageViews));
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
 }
